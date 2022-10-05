4169ca92566faaed52cef95d7a90cba73ac81832
Browyn Louis <browynlouis2@gmail.com>
2022-10-04 20:39:40 -0400

    Final Deployment of Review Rating App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from "react";
 
-import './App.scss';
+import icon from "./assets/images/icon-star.svg";
+import iconThankYou from "./assets/images/illustration-thank-you.svg";
+import "./App.scss";
 
 function App() {
 
+  const [select, setSelect] = React.useState(0);
+  const [submitMessage, setSubmitMessage] = React.useState(false);
+  const ratings = [1, 2, 3, 4, 5];
+  const submit = (e) => {
+    e.preventDefault(0);
+    return select === 0 ? null : setSubmitMessage(!false);
+  };
+
   return (
     <>
+      <div className="card-container">
+        <div className="card-body">
+          {!submitMessage &&
+            <div className="card-logo">
+              <img src={icon} alt="Star Icon" />
+            </div>}
+          <div className="card-content">
+            {!submitMessage ?
+              <>
+                <h1>How did we do?</h1>
+                <p>Please let us know how we didi with your support request. All feedback is appreciated to help us improve our offering!</p>
+                <div className="ratings">
+                  {ratings.map((rating) => (
+                    <span key={`option-${rating}`} onClick={(e) => {
+                      e.preventDefault();
+                      setSelect(rating);
+                      console.log(rating);
+                    }} className={select === rating ? "selected" : null}>{rating}</span>
+                  ))}
+                </div>
+              </>
+              :
+              <div className="card-appreciation">
+                <img src={iconThankYou} alt="Thank you illustration" />
+                <h1>Thank You!</h1>
+                <span>You selected {select} out of 5</span>
+                <p>We appreciate you taking the time to give a rating.
+                  If you ever need support, don't hesitate to get in touch!</p>
+              </div>
 
+            }
+            {!submitMessage && <button type="button" onClick={submit}>SUBMIT</button>}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
